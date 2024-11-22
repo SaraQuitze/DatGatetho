1939bb8d668dd310d925ae16e93b1ca43e7bdd56
Belladonna <102000211+SaraQuitze@users.noreply.github.com>
2024-11-22 16:00:32 -0500

    Add files via upload

---
+++ homeworkDoor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour
+{
+    //for the switch of the door
+    
+    private BoxCollider bc;
+    private SpriteRenderer sp;
+    private GameObject indicatorCanInteract;//a signal for the player to know with what can interact
+    private Animator anim
+    private bool canInteract;;
+    public bool isSwitch;
+    public bool ActiveSwitch;
+    public UnityEvent switchEvent;
+
+    private void Awake() //calling the components
+    {
+        bc = GetComponent<BoxCollider>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if(transform.GetChild(0) != null)
+        {
+            indicatorCanInteract = transform.GetChild(0).gameObject;
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        //when the collider gets tag player, it'll able the interaction with the switch
+        if(collision.CompareTag("Player"))
+        {
+            canInteract = true;
+            indicatorCanInteract.SetActive(true);
+        }
+    }
+    private void Switch()
+    {
+        if(isSwitch && !ActiveSwitch)
+        {
+            anim.SetBool("active", true);
+            ActiveSwitch = true;
+            switchEvent.Invoke();
+            indicatorCanInteract.SetActive(false);
+            bc.enabled = false;
+            this.enabled = false;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(canInteract && Input.GetKeyDown(KeyCode.C))
+        {
+            Switch();
+        }
+    }
+}
